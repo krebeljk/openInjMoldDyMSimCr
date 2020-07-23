d1ec5586aab0ff5a7c3e13c534fc533c5bda3995
Kristjan <krebeljk@gmail.com>
2020-07-23 20:22:24 +0200

    openInjMoldSim.C: ;

--- applications/solvers/multiphase/openInjMoldSim/openInjMoldSim.C
+++ applications/solvers/multiphase/openInjMoldSim/openInjMoldSim.C
@@ -95,9 +95,9 @@ int main(int argc, char *argv[])
             //Kristjan: Elastic deviatoric stress equation
             if (sldDictIO.headerOk())
             {
-                volScalarField sigSmStop1 = -(shrRateLimEl-shrRate)/(1e-3*shrRateLimEl)
+                volScalarField sigSmStop1 = -(shrRateLimEl-shrRate)/(1e-3*shrRateLimEl);
                 sigSmStop1 = min(max(sigSmStop1 ,0.0),1.0);
-                volScalarField sigSmStop2 = -(visc-viscLimEl)/(1e-3*viscLimEl)
+                volScalarField sigSmStop2 = -(visc-viscLimEl)/(1e-3*viscLimEl);
                 sigSmStop2 = min(max(sigSmStop2 ,0.0),1.0);
 
                 fvSymmTensorMatrix elSigDevEqn(
